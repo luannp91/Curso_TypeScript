6429d41c4b25209e6abf32d304ec3998e01986f0
Luan Pinto <luan.nelmo@hotmail.com>
2022-07-01 19:06:57 -0300

    Iniciando o Express

--- 12_Express_com_Typescript/src/app.ts
+++ 12_Express_com_Typescript/src/app.ts
@@ -1,2 +1,16 @@
 //  1   -   Integração TS + Express
 console.log("Express + TS");
+
+
+//  2   -   Iniciando o Express
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+    return res.send("Hello Express!!!")
+})
+
+app.listen(3000, () => {
+    console.log("Aplicação de TypeScript + Express funcionando!!!");    
+})
