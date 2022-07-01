be0a2f569695d368f3b68e8e5214c1b16fde79d8
Luan Pinto <luan.nelmo@hotmail.com>
2022-07-01 19:16:44 -0300

    Aula sobre Rota com POST

--- 12_Express_com_Typescript/src/app.ts
+++ 12_Express_com_Typescript/src/app.ts
@@ -1,14 +1,19 @@
-//  1   -   Integração TS + Express
 console.log("Express + TS");
 
-
-//  2   -   Iniciando o Express
 import express from "express";
 
 const app = express();
 
+app.use(express.json())
+
 app.get("/", (req, res) => {
     return res.send("Hello Express!!!")
+})
+
+app.post("/api/product", (req, res) => {
+    console.log(req.body);
+    
+    return res.send("Produto adicionado!!!")
 })
 
 app.listen(3000, () => {
