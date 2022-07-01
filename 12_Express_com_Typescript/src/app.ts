6d94773ce348e026520c8472cebba29a635a18d8
Luan Pinto <luan.nelmo@hotmail.com>
2022-07-01 19:22:41 -0300

    Aula sobre Rota para qualquer verbo

--- 12_Express_com_Typescript/src/app.ts
+++ 12_Express_com_Typescript/src/app.ts
@@ -16,6 +16,18 @@ app.post("/api/product", (req, res) => {
     return res.send("Produto adicionado!!!")
 })
 
+app.all("/api/product/check", (req, res) => {
+    if (req.method === "POST") {
+        return res.send("Inseriu algum registro!!!")
+    }
+    else if (req.method === "GET") {
+        return res.send("Leu algum registro!!!")
+    }
+    else {
+        return res.send("Não podemos realizar esta operação!!!")
+    }
+})
+
 app.listen(3000, () => {
     console.log("Aplicação de TypeScript + Express funcionando!!!");    
 })
