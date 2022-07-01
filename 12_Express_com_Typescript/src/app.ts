16da962b784b4b8f6a858beb02fa409c9152d194
Luan Pinto <luan.nelmo@hotmail.com>
2022-07-01 19:26:53 -0300

    Aula sobre interfaces do Express

--- 12_Express_com_Typescript/src/app.ts
+++ 12_Express_com_Typescript/src/app.ts
@@ -1,6 +1,6 @@
 console.log("Express + TS");
 
-import express from "express";
+import express, { Request, Response } from "express";
 
 const app = express();
 
@@ -26,6 +26,10 @@ app.all("/api/product/check", (req, res) => {
     else {
         return res.send("Não podemos realizar esta operação!!!")
     }
+})
+
+app.get("/api/interfaces", (req: Request, res: Response) => {
+    return res.send("Utilizando as interfaces")
 })
 
 app.listen(3000, () => {
