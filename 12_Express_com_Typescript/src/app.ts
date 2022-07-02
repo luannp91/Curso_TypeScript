62a41bd0631c8ed9d876586f5a54e7520c624d22
Luan Pinto <luan.nelmo@hotmail.com>
2022-07-01 21:26:58 -0300

    Aula sobre Rotas mais complexas

--- 12_Express_com_Typescript/src/app.ts
+++ 12_Express_com_Typescript/src/app.ts
@@ -61,6 +61,15 @@ app.get("/api/product/:id", (req: Request, res: Response) => {
     }    
 })
 
+app.get("/api/product/:id/review/:reviewId", (req: Request, res: Response) => {
+    console.log(req.params);
+    
+    const productId = req.params.id
+    const reviewId = req.params.reviewId;
+
+    return res.send(`Acessando a review ${reviewId} do produto ${productId}`)
+})
+
 app.listen(3000, () => {
     console.log("Aplicação de TypeScript + Express funcionando!!!");    
 })
